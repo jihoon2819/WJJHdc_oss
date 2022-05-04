b22ead54f350e7353a773d6f76826bc595142d27
j_h___ <85489123+jihoon2819@users.noreply.github.com>
2022-05-04 11:00:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup (
+    include_package_data = True,
+    name = 'jh_cacl',
+    version = '1.0.0',
+    description='oss-dev jihoon's calculator',
+    author = 'jihoon2819',
+    author_email = 'palkj2580477@naver.com'
+    url = "https://github.com/jihoon2819/WJJHdc_oss/",
+    download_url = "https://github.com/jihoon2819/WJJHdc_oss/archive/refs/tags/v1.0.0.zip"
+    install_requires = ['pytest', 'pandas'],
+    long_description = 'oss-dev calculator python module',
+    long_description-content_type = 'text/markdown',
+    classifiers=[
+      "programming Language :: Python :: 3",
+      "Operating System :: OS Independent",
+    ],
